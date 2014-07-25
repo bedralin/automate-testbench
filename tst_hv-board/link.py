2e8e46f93a67e53dc5d0e252374524416f2b48a0
Bronson Edralin <bedralin@hawaii.edu>
2014-07-24 16:58:09 -1000

    Updated link.py with proper delays.

--- tst_hv-board/link.py
+++ tst_hv-board/link.py
@@ -71,11 +71,11 @@ class Ethernet:
 
 # Ethernet_Controller class used to access Instrument through GPIB via Ethernet
 class Ethernet_Controller:
-    def __init__(self, addr=None, port=None):
+    def __init__(self, addr=None, port=None, delay=0.5, timeout=0.5):
 	self.addr = str(addr)    
 	self.mutex = Lock()
-	self.delay = 0.5
-	self.timeout = 0.5
+	self.delay = float(delay)
+	self.timeout = float(timeout)
 	try:
 	    self.port = int(port)
 	except TypeError:
@@ -212,7 +212,7 @@ class Ethernet_Controller:
 #RS232 Class is used to access Instrument through RS-232 via USB.
 class RS232:
     def __init__(self,port='/dev/ttyUSB0',baudrate=9600,databits=8, \
-		parity='None',stopbits=1,timeout=3,xonxoff=False,rtscts=False):
+		parity='None',stopbits=1,timeout=1,xonxoff=False,rtscts=False,delay=0.5):
 	self.port = port  # Device name or port #
 	self.baudrate = int(baudrate)  # Baud rate such as 9600
 	self.databits = int(databits)  # Number of databits such as 5,6,7,8
@@ -233,7 +233,7 @@ class RS232:
 	self.timeout = float(timeout)    # Set read timeout
 	#self.xonxoff = xonxoff  # Enable software flow control: True/False
 	#self.rtscts = rtscts  # Enable hardware (RTS/CTS) flow control: True/False
-	self.delay = 1 
+	self.delay = float(delay)
 
         #Open the serial port
         self.ser=serial.Serial(
